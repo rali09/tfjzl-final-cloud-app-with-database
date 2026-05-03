3b84aa974ed2a706015c6864ccb0c9f318f19bf5
rali09 <ralieka.22310537@viit.ac.in>
2026-05-03 15:32:12 -0400

    final django project

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,38 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
-class LessonInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 5
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class LessonInline(admin.TabularInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
 
-
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
-
-
-# <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,6 +94,34 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    grade = models.FloatField(default=50.0)
+
+    def is_get_score(self, selected_ids):
+        correct_ids = self.choice_set.filter(is_correct=True).values_list('id', flat=True)
+        return set(correct_ids) == set(selected_ids)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f"{self.enrollment.user} - {self.enrollment.course}"
 
 # One enrollment could have multiple submission
 # One submission could have multiple choices
